1af255b2df6d63ea4c3667dbba8aa1b3b1dc8e19
ZanderSensei <109417236+ZanderSensei@users.noreply.github.com>
2024-02-13 12:03:34 +0300

    Delete admin directory

--- admin/includes/auth.php
+++
@@ -1,8 +0,0 @@
-<?php 
-$self = $_SERVER['PHP_SELF'];
-if(!strpos($self, "admin/login.php") && !strpos($self, "admin/registration.php")){
-    if(!isset($_SESSION['userdata']['id'])){
-        redirect('./admin/login.php');
-    }
-}
-?>
